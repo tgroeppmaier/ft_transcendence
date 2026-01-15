e9358ebd03be9c65285721b21dc53ddaada55944
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2026-01-15 08:49:11 +0100

    add LocalGame class

---
+++ frontend/src/utils/localGame.ts
@@ -0,0 +1,190 @@
+import {
+  CANVAS_WIDTH,
+  CANVAS_HEIGHT,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  PADDLE_SPEED,
+  BALL_X_SPEED,
+  BALL_Y_SPEED,
+  BALL_RADIUS,
+  POINTS_TO_WIN,
+  MAX_BALL_SPEED,
+} from "../../../shared/constants.js";
+import { Ball, Paddle, Score } from "../../../shared/types.js";
+import { drawBall, drawPaddles, drawMessage, drawScores } from "../utils/gameRenderer.js";
+
+export class LocalGame {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  private ball: Ball = { x: 0.5, y: 0.5, vx: BALL_X_SPEED, vy: BALL_Y_SPEED };
+  private score: Score = { left: 0, right: 0};
+  private lastUpdate: number;
+  private keyMap: Record<string, boolean> = { "w": false, "s": false, "ArrowUp": false, "ArrowDown": false };
+  private player1: string;
+  private player2: string;
+  private leftPaddle: Paddle;
+  private rightPaddle: Paddle;
+  private gameOver = false;
+  private countdown = 5;
+  private gameStarted = false;
+  private rafID: number;
+
+  constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, player1?: string, player2?: string) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.lastUpdate = Date.now();
+    this.player1 = player1 ?? "Player1";
+    this.player2 = player2 ?? "Player2";
+    this.resetPaddles();
+  }
+
+  public start() {
+    this.lastUpdate = performance.now();
+    this.rafID = requestAnimationFrame(this.tick);
+  }
+
+  public stop() {
+    cancelAnimationFrame(this.rafID);
+  }
+
+  public onKeyDown(key: string) {
+    if (key in this.keyMap) this.keyMap[key] = true;
+  }
+
+  public onKeyUp(key: string) {
+    if (key in this.keyMap) this.keyMap[key] = false;
+  }
+
+  public resetGame = () => {
+    if (this.gameOver) {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.score.left = 0;
+      this.score.right = 0;
+      this.resetPaddles();
+      this.resetBall();
+      cancelAnimationFrame(this.rafID);
+      this.gameOver = false;
+      this.lastUpdate = performance.now();
+      this.render()
+      this.rafID = requestAnimationFrame(this.tick);
+    }
+  }
+
+    private resetBall() {
+    this.ball.x = CANVAS_WIDTH / 2;
+    this.ball.y = CANVAS_HEIGHT / 2;
+    this.ball.vx = this.ball.vx > 0 ? -BALL_X_SPEED : BALL_X_SPEED;
+    this.ball.vy = (Math.random() - 0.5) * BALL_Y_SPEED * 3;
+  }
+
+  private resetPaddles() {
+    this.leftPaddle = { x: 0, y: CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2 }
+    this.rightPaddle = { x: CANVAS_WIDTH - PADDLE_WIDTH, y: CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2 };
+  }
+
+
+
+private render() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    if (!this.gameStarted) {
+      drawMessage(this.ctx, this.canvas, `${this.player1}: W/S keys | ${this.player2}: Arrow Up/Down` , this.canvas.height / 3);
+      drawMessage(this.ctx, this.canvas, `Game starts in ${Math.ceil(this.countdown)}`, this.canvas.height / 2);
+    }
+    else {
+      drawBall(this.ctx, this.canvas, this.ball);
+      drawPaddles(this.ctx, this.canvas, this.leftPaddle, this.rightPaddle);
+      drawScores(this.ctx, this.canvas, this.score);
+    }
+  }
+
+  private bouncePaddle(paddleY: number) {
+    this.ball.vx = -this.ball.vx * 1.05;
+    if (Math.abs(this.ball.vx) > MAX_BALL_SPEED) {
+      this.ball.vx = this.ball.vx > 0 ? MAX_BALL_SPEED : -MAX_BALL_SPEED;
+    }
+    if (this.ball.y < paddleY + PADDLE_HEIGHT * 0.25) {
+      this.ball.vy = -Math.abs(this.ball.vy) - 0.01;
+    } else if (this.ball.y > paddleY + PADDLE_HEIGHT * 0.75) {
+      this.ball.vy = Math.abs(this.ball.vy) + 0.01;
+    }
+  }
+
+  private handlePaddleCollision() {
+    const checkYaxis = (paddle: Paddle) => {
+      return (this.ball.y + BALL_RADIUS >= paddle.y && this.ball.y - BALL_RADIUS <= paddle.y + PADDLE_HEIGHT);
+    };
+
+    if (this.ball.vx < 0 && checkYaxis(this.leftPaddle) &&
+      this.ball.x - BALL_RADIUS < this.leftPaddle.x + PADDLE_WIDTH) {
+      this.ball.x = this.leftPaddle.x + PADDLE_WIDTH + BALL_RADIUS;
+      this.bouncePaddle(this.leftPaddle.y);
+    } else if (this.ball.vx > 0 && checkYaxis(this.rightPaddle) &&
+      this.ball.x + BALL_RADIUS > this.rightPaddle.x) {
+      this.ball.x = this.rightPaddle.x - BALL_RADIUS;
+      this.bouncePaddle(this.rightPaddle.y);
+    }
+  }
+
+  private handlePaddleMovement(dt: number) {
+    if (this.keyMap["w"]) this.leftPaddle.y -= PADDLE_SPEED * dt;
+    if (this.keyMap["s"]) this.leftPaddle.y += PADDLE_SPEED * dt;
+    if (this.keyMap["ArrowUp"]) this.rightPaddle.y -= PADDLE_SPEED * dt;
+    if (this.keyMap["ArrowDown"]) this.rightPaddle.y += PADDLE_SPEED * dt;
+
+    this.leftPaddle.y = Math.max(0, Math.min(CANVAS_HEIGHT - PADDLE_HEIGHT, this.leftPaddle.y));
+    this.rightPaddle.y = Math.max(0, Math.min(CANVAS_HEIGHT - PADDLE_HEIGHT, this.rightPaddle.y));
+  }
+
+  private handleWallCollision() {
+    if (this.ball.y + BALL_RADIUS > CANVAS_HEIGHT) {
+      this.ball.y = CANVAS_HEIGHT - BALL_RADIUS;
+      this.ball.vy *= -1;
+    }
+    if (this.ball.y - BALL_RADIUS < 0) {
+      this.ball.y = BALL_RADIUS;
+      this.ball.vy *= -1;
+    }
+  }
+
+  private handleScore() {
+    if (this.ball.x < 0) {
+      this.resetBall();
+      this.score.right += 1;
+    } else if (this.ball.x > CANVAS_WIDTH) {
+      this.resetBall();
+      this.score.left += 1;
+    }
+  }
+
+  private tick = (now: number) => {
+    const dt = (now - this.lastUpdate) / 1000;
+    this.lastUpdate = now;
+    if (!this.gameStarted) {
+      this.countdown -= dt;
+      if (this.countdown > 0) {
+        this.render();
+        this.rafID = requestAnimationFrame(this.tick);
+        return;
+      } else this.gameStarted = true;
+    }
+    this.ball.x += this.ball.vx * dt;
+    this.ball.y += this.ball.vy * dt;
+
+    this.handlePaddleMovement(dt);
+    this.handlePaddleCollision();
+    this.handleWallCollision();
+    this.handleScore();
+
+    if (this.score.left >= POINTS_TO_WIN || this.score.right >= POINTS_TO_WIN) {
+      this.gameOver = true;
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      drawScores(this.ctx, this.canvas, this.score);
+      drawMessage(this.ctx, this.canvas, this.score.left > this.score.right ? `${this.player1} wins` : `${this.player2} wins`);
+      drawMessage(this.ctx, this.canvas, "click to play again", (this.canvas.height / 3) * 2);
+      return;
+    }
+    this.render();
+    this.rafID = requestAnimationFrame(this.tick);
+  }
+}
--- frontend/src/views/LocalGameView.ts
+++ frontend/src/views/LocalGameView.ts
@@ -1,18 +1,5 @@
 import { navigateTo } from "../router.js";
-import { Ball, Paddle, Score } from "../../../shared/types.js";
-import { drawBall, drawPaddles, drawMessage, drawScores } from "../utils/gameRenderer.js";
-import {
-  CANVAS_WIDTH,
-  CANVAS_HEIGHT,
-  PADDLE_WIDTH,
-  PADDLE_HEIGHT,
-  PADDLE_SPEED,
-  BALL_X_SPEED,
-  BALL_Y_SPEED,
-  BALL_RADIUS,
-  POINTS_TO_WIN,
-  MAX_BALL_SPEED,
-} from "../../../shared/constants.js";
+import { LocalGame } from "../utils/localGame.js";
 
 export function LocalGameView() {
   const gameContainer = document.createElement("div");
@@ -43,153 +30,16 @@ export function LocalGameView() {
   if (!ctx)
     throw new Error("Context not found");
 
-  // Game state
-  const ball: Ball = { x: CANVAS_WIDTH / 2, y: CANVAS_HEIGHT / 2, vx: BALL_X_SPEED, vy:BALL_Y_SPEED };
-  const leftPaddle: Paddle = { x: 0, y: CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2 };
-  const rightPaddle: Paddle = { x: CANVAS_WIDTH - PADDLE_WIDTH, y: CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2 };
-  const score: Score = { left: 0, right: 0 };
-  const keyMap: Record<string, boolean> = { "w": false, "s": false, "ArrowUp": false, "ArrowDown": false };
-  let gameOver = false;
-  let countdown = 7;
-  let gameStarted = false;
-
-  function resetBall() {
-    ball.x = CANVAS_WIDTH / 2;
-    ball.y = CANVAS_HEIGHT / 2;
-    ball.vx = ball.vx > 0 ? -BALL_X_SPEED : BALL_X_SPEED;
-    ball.vy = (Math.random() - 0.5) * BALL_Y_SPEED * 3;
-  }
-
-  const resetPaddles = () => {
-    leftPaddle.y = CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2;
-    rightPaddle.y = CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2
-  }
-
-  const resetGame = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    score.left = 0;
-    score.right = 0;
-    resetPaddles();
-    resetBall();
-    cancelAnimationFrame(rafID);
-    gameOver = false;
-    last = performance.now();
-    render()
-    rafID = requestAnimationFrame(tick);
-  }
-
-  function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (!gameStarted) {
-      drawMessage(ctx, canvas, "Player 1: W/S keys | Player 2: Arrow Up/Down", canvas.height / 3);
-+     drawMessage(ctx, canvas, `Game starts in ${Math.ceil(countdown)}`, canvas.height / 2);
-    }
-    else {
-      drawBall(ctx, canvas, ball);
-      drawPaddles(ctx, canvas, leftPaddle, rightPaddle);
-      drawScores(ctx, canvas, score);
-    }
-  }
-
-  // stores identifier of requestAnimationFrame
-  let rafID = 0;
-  // get timestamp
-  let last = performance.now();
-
-  function bouncePaddle(paddleY: number) {
-  ball.vx = -ball.vx * 1.05;
-  if (Math.abs(ball.vx) > MAX_BALL_SPEED) {
-    ball.vx = ball.vx > 0 ? MAX_BALL_SPEED : -MAX_BALL_SPEED;
-  }
-  if (ball.y < paddleY + PADDLE_HEIGHT * 0.25) {
-    ball.vy = -Math.abs(ball.vy) - 0.01;
-  } else if (ball.y > paddleY + PADDLE_HEIGHT * 0.75) {
-    ball.vy = Math.abs(ball.vy) + 0.01;
-    }
-  }
-
-  function handlePaddleCollision() {
-    const checkYaxis = (paddle: Paddle) => {
-      return (ball.y + BALL_RADIUS >= paddle.y && ball.y - BALL_RADIUS <= paddle.y + PADDLE_HEIGHT);
-    };
-
-    if (ball.vx < 0 && checkYaxis(leftPaddle) &&
-      ball.x - BALL_RADIUS < leftPaddle.x + PADDLE_WIDTH) {
-      ball.x = leftPaddle.x + PADDLE_WIDTH + BALL_RADIUS;
-      bouncePaddle(leftPaddle.y);
-    } else if (ball.vx > 0 && checkYaxis(rightPaddle) &&
-      ball.x + BALL_RADIUS > rightPaddle.x) {
-      ball.x = rightPaddle.x - BALL_RADIUS;
-      bouncePaddle(rightPaddle.y);
-    }
-  }
-
-  const handleScore = () => {
-    if (ball.x < 0) {
-      resetBall();
-      score.right += 1;
-    } else if (ball.x > CANVAS_WIDTH) {
-      resetBall();
-      score.left += 1;
-    }
-  }
-
-  function tick(now: number) {
-    const dt = (now - last) / 1000;
-    last = now;
-    if (!gameStarted) {
-      countdown -= dt;
-      if (countdown > 0) {
-        render();
-        rafID = requestAnimationFrame(tick);
-        return;
-      }
-      else gameStarted = true;
-    }
-    if (keyMap["w"]) leftPaddle.y -= PADDLE_SPEED * dt;
-    if (keyMap["s"]) leftPaddle.y += PADDLE_SPEED * dt;
-    if (keyMap["ArrowUp"]) rightPaddle.y -= PADDLE_SPEED * dt;
-    if (keyMap["ArrowDown"]) rightPaddle.y += PADDLE_SPEED * dt
-
-    // clamp to top and bottom. paddle y should be min 0 and max canvas.height - paddleH
-    leftPaddle.y = Math.max(0, Math.min(CANVAS_HEIGHT - PADDLE_HEIGHT, leftPaddle.y));
-    rightPaddle.y = Math.max(0, Math.min(CANVAS_HEIGHT - PADDLE_HEIGHT, rightPaddle.y));
-
-    ball.x += ball.vx * dt;
-    ball.y += ball.vy * dt;
-
-    handlePaddleCollision();
-    handleScore();
-
-    // top can bottom Wall colission
-    if (ball.y + BALL_RADIUS > CANVAS_HEIGHT) { ball.y = CANVAS_HEIGHT - BALL_RADIUS; ball.vy *= -1; }
-    if (ball.y - BALL_RADIUS < 0) { ball.y = BALL_RADIUS; ball.vy *= -1; }
-
-    if (score.left >= POINTS_TO_WIN || score.right >= POINTS_TO_WIN) {
-      gameOver = true;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawScores(ctx, canvas, score);
-      drawMessage(ctx, canvas, score.left > score.right ? "Player 1 wins" : "Player 2 wins");
-      drawMessage(ctx, canvas, "click to play again", (canvas.height / 3) * 2);
-      return;
-    }
-
-    render();
-    rafID = requestAnimationFrame(tick);
-  }
-
-  // event listener callback functions
-  const onCanvasClick = () => {
-    if(gameOver)
-      resetGame();
-  }
-
   const onKeyDown = (e: KeyboardEvent) => {
-    if (e.key in keyMap) keyMap[e.key] = true;
+    game.onKeyDown(e.key);
   }
 
   const onKeyUp = (e: KeyboardEvent) => {
-    if (e.key in keyMap) keyMap[e.key] = false;
+    game.onKeyUp(e.key);
+  }
+
+  const onCanvasClick = () => {
+    game.resetGame();
   }
 
   // adding event Listeners
@@ -197,11 +47,11 @@ export function LocalGameView() {
   document.addEventListener("keyup", onKeyUp);
   canvas.addEventListener("click", onCanvasClick);
 
-  // // // calls callback function tick and passes in timestamp
-  rafID = requestAnimationFrame(tick);
+  const game = new LocalGame(canvas, ctx);
+  game.start();
 
   const cleanup = () => {
-    cancelAnimationFrame(rafID);
+    game.stop();
     document.removeEventListener("keydown", onKeyDown);
     document.removeEventListener("keyup", onKeyUp);
     canvas.removeEventListener("click", onCanvasClick);
