c87bf90577e24852d67b7ffe476047ec5afdfb21
Olga Lomova <olomova@c1r3s7.42prague.com>
2026-01-31 18:20:00 +0100

    specify fontFamily to suppress browser font warnings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,ts}",
-    "./*.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./src/**/*.{html,ts}",
+		"./*.html"
+	],
+	theme: {
+		fontFamily: {
+			sans: [
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				"Segoe UI",
+				"Arial",
+				"sans-serif",
+			],
+			mono: [
+				"ui-monospace",
+				"SFMono-Regular",
+				"Menlo",
+				"Consolas",
+				"monospace",
+			],
+		},
+	},
+	plugins: [],
 }
+
